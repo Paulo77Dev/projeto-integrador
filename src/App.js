fe564b1c890481087f3ea52750197c7fb49ed5cf
Paulo77Dev <paulomadson16@gmail.com>
2024-11-20 14:29:16 -0300

    Alteraciones del archivo raíz de la aplicación App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DOMNative from "./components/Section1/DOMNative";
+import DOMReact from "./components/Section1/DOMReact";
+import Gallery from "./components/Section2/Gallery";
+import GSAPAnimation from "./components/Section3/GSAPAnimation";
+import AnimeAnimation from "./components/Section4/AnimeAnimation";
+import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Trabalho Integrador</h1>
+            <DOMNative />
+            <DOMReact />
+            <Gallery />
+            <GSAPAnimation />
+            <AnimeAnimation />
+        </div>
+    );
 }
 
 export default App;
